aa80f2066fdff0ed368db9bea38ae69d901445ea
AOMIDORI <576049934@qq.com>
2016-04-16 01:06:29 +0200

    Update Database.h

--- Database.h
+++ Database.h
@@ -6,16 +6,22 @@
 #include <vector>
 #include <string>
 #include <ctime>
+using namespace std;
 
-typedef struct{
+
+typedef struct User;
+typedef struct Message;
+struct User{
 	char *name;
 	int status; // 0: offline,  1: online
-}User;
+	vector<Message> messagelist;
+	vector<User> following;
+};
 
+struct Message{
+	int messageID;
+	struct tm *publishtime;
+	char *text;
+};
 
-void message_handler(int option){
-
-}
-//void follow_handler(int option){}
-//void print_timeline(char *username){}
 #endif
